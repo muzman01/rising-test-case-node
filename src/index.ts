d88bdf9e74a31cd5b6c41496c9b939d3262ae473
muzman01 <supurge_man@hotmail.com>
2024-09-21 13:52:49 +0200

    add models relationship and fix struct

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("selam");
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { sequelize } from "./models"; // Tüm modeller burada import edilir
+
+const app = express();
+
+app.use(express.json());
+
+sequelize.sync({ force: false }).then(() => {
+  console.log("Veritabanı senkronize edildi.");
+
+  // Sunucuyu başlat
+  app.listen(3000, () => {
+    console.log("Sunucu 3000 portunda çalışıyor.");
+  });
+});
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,68 @@
-import { Sequelize } from "sequelize";
+import sequelize from "./sequelize"; // Sequelize bağlantısı
+import { DataTypes } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite", // Veritabanı dosyanın konumu
+// User Modeli
+const User = sequelize.define("User", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  balance: {
+    type: DataTypes.INTEGER,
+    defaultValue: 100,
+  },
 });
 
-export default sequelize;
+// Service Modeli
+const Service = sequelize.define("Service", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
+
+// Order Modeli
+const Order = sequelize.define("Order", {
+  serviceName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  totalPrice: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
+
+// İlişkileri tanımlıyoruz
+User.hasMany(Order, { foreignKey: "userId", as: "orders" });
+Order.belongsTo(User, { foreignKey: "userId", as: "user" });
+
+Order.belongsTo(Service, { foreignKey: "serviceId", as: "service" });
+
+// Tüm modelleri export edelim
+export { sequelize, User, Order, Service };
