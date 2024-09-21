c45d653ca0f4a91cbb43bb71378a42b4f2a85a75
muzman01 <supurge_man@hotmail.com>
2024-09-21 14:09:52 +0200

    add errors and middlewares

---
+++ src/errors/errorHandler.ts
@@ -0,0 +1,19 @@
+// middlewares/errorHandler.ts
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/customError";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  console.error(err);
+  res.status(500).send({
+    errors: [{ message: "Bilinmeyen bir hata oluştu" }],
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import session from "express-session";
 import "express-async-errors";
-import cookieSession from "cookie-session";
+import "dotenv/config";
 
+
+// services
 import { sequelize } from "./models"; // Tüm modeller burada import edilir
+import { errorHandler } from "./errors/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+// Oturum yönetimi için session middleware'i
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!, // Oturum için gizli anahtar
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(errorHandler);
 
 sequelize.sync({ force: false }).then(() => {
   console.log("Veritabanı senkronize edildi.");
